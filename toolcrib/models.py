97e39ca432e9c37d1b2fb44c7ca82e8be21bd7ab
Carlos Iturrios <c.iturriosalcaraz@gmail.com>
2018-04-26 09:28:13 -0700

    Crear clase Part y agregar libreria para control de imagenes

--- toolcrib/models.py
+++ toolcrib/models.py
@@ -4,3 +4,28 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Part(models.Model):
+	num_part = models.CharField(max_length=70, unique=True, null=False, blank=False)
+	description = models.CharField(max_length=100, db_index=True, null=False, blank=False)
+	inventory_type = models.CharField(max_length=100, db_index=True)
+	image = models.ImageField(upload_to='toolcrib/static/media', null=True, blank=False)	
+	category = models.CharField(
+		max_length=1, blank=False, default='1', choices=(
+			('1','SUPPLY'),
+			('2','EXPERT PARTS')
+		)
+	)
+	clas = models.CharField(
+		max_length = 1, blank=False, default='1', choices =(
+			('1','MXN'),
+			('2','USD')
+		)
+	)
+	location = models.CharField(max_length=200, null=True, blank=True)
+	price = models.DecimalField(max_digits=10, decimal_places=4, null=False, blank=False)
+	quantity = models.PositiveIntegerField(null=False, blank=False)
+	max_quantity = models.PositiveIntegerField(null=False, blank=False)
+	min_quantity = models.PositiveIntegerField(null=False, blank=False)
+
+	def __str__(self):
+		return self.num_part
--- toolcrib/views.py
+++ toolcrib/views.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def principal(request):
 	return render(request, 'base.html')
