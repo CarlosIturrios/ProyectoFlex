125c0126b520bcb1ed4c2dba5c298e24830f1888
Carlos Iturrios <c.iturriosalcaraz@gmail.com>
2018-04-26 11:31:10 -0700

    Agregar los modelos faltantes (error en modelo pedidos)

--- toolcrib/models.py
+++ toolcrib/models.py
@@ -32,3 +32,38 @@ class Part(models.Model):
 	def __str__(self):
 		return self.num_part
 
+
+class Order(models.Model):
+	date_order = models.DateTimeField(null=True, blank=True)
+	status = models.CharField(
+		max_length = 1, blank=False, default='1', choices =(
+			('1','PENDIENTE'),
+			('2','APROBADO'),
+			('3','CANCELADO'),
+			('4','REALIZADO')
+		)
+	)
+	level = models.CharField(
+		max_length = 1, blank=False, default='1', choices =(
+			('1','NORMAL'),
+			('2','NO ES URGENTE'),
+			('3','URGENTE')
+		)
+	)
+	cost_center = models.CharField(max_length=100, null=False, blank=False)
+	comments = models.CharField(max_length=200, null=True, blank=True)
+	user = models.ForeignKey(User, unique=False)
+	supervisor = models.ForeignKey(User, unique=False)
+	date_done = models.DateTimeField(null=True, blank=True)
+
+	def __str__(self):
+		return self.status
+
+
+class Order_Details(models.Model):
+	order = models.ForeignKey(Order, on_delete=models.CASCADE)
+	num_part = models.ForeignKey(Part, on_delete=models.CASCADE)
+	quantity = models.PositiveIntegerField(null=False, blank=False)
+	
+	def __str__(self):
+		return self.order
