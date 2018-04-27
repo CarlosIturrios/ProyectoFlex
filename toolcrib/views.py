63ff76aa0644844898692de243abbafd9f135ae2
Carlos Iturrios <c.iturriosalcaraz@gmail.com>
2018-04-27 08:11:00 -0700

    Mostrar vista ordersmanager

--- toolcrib/views.py
+++ toolcrib/views.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.utils.decorators import method_decorator
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
 @login_required
 def principal(request):
 	return render(request, 'principal.html')
 
-def ordersmanager(request):
+@permission_required('Part.can_add')
+def ordersmanager(request):	
 	return render(request, 'ordersmanager.html')
 
 def orderssupervisor(request):
