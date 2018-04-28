14b77b91fbd438a2165a7db2eb0d48a9faa303ec
Luis Iturrios <iturrios3063@gmail.com>
2018-04-28 13:55:20 -0700

    Fix perms errors

--- toolcrib/urls.py
+++ toolcrib/urls.py
@@ -6,15 +6,14 @@ from . import views
 urlpatterns = [
     
     # Auth Urls
-    url(r'login/$', auth_views.login, name='login'),
-    url(r'logout/$', auth_views.logout, {'next_page': '/toolcrib/login/'}, name='logout'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/toolcrib/login/'}, name='logout'),
 
     # App views    
-   	url(r'ordersmanager/$', views.ordersmanager, name='ordersmanager'),
- 	url(r'orderssupervisor/$', views.orderssupervisor, name='orderssupervisor'),  
- 	url(r'updateproduct/$', views.updateproduct, name='updateproduct'),	
- 	url(r'updateuser/$', views.updateuser, name='updateuser'),
- 	url(r'products/$', views.products, name='products'),
- 	url(r'$', views.principal, name='principal'),
-
+   	url(r'^$', views.principal, name='principal'),
+   	url(r'^ordersmanager/$', views.ordersmanager, name='ordersmanager'),
+ 	url(r'^orderssupervisor/$', views.orderssupervisor, name='orderssupervisor'),  
+ 	url(r'^updateproduct/$', views.updateproduct, name='updateproduct'),	
+ 	url(r'^updateuser/$', views.updateuser, name='updateuser'),
+ 	url(r'^products/$', views.products, name='products'),
 ]
--- toolcrib/views.py
+++ toolcrib/views.py
@@ -1,26 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.utils.decorators import method_decorator
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
-@login_required
+@login_required()
 def principal(request):
 	return render(request, 'principal.html')
 
-@permission_required('Part.can_add')
+
+@login_required()
+@permission_required('toolcrib.add_part')
 def ordersmanager(request):	
 	return render(request, 'ordersmanager.html')
 
+
+@login_required()
 def orderssupervisor(request):
 	return render(request, 'orderssupervisor.html')
 
+
+@login_required()
 def updateproduct(request):
 	return render(request, 'updateproduct.html')
 
+
+@login_required()
 def updateuser(request):
 	return render(request, 'updateuser.html')
 
+
+@login_required()
 def products(request):
 	return render(request, 'products.html')
