4ed9394c1b24cafe030f3bcfb491e0936a470564
Carlos Iturrios <c.iturriosalcaraz@gmail.com>
2018-04-30 05:39:53 -0700

    Filtrado de busqueda y paginacion, template del shopping cart

--- toolcrib/views.py
+++ toolcrib/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required, permission_required
 
 from .models import Part
@@ -35,8 +36,32 @@ def updateuser(request):
 
 @login_required()
 def parts(request):
-	parts = Part.objects.all()
-	return render(request, 'parts.html', {'parts' : parts})
+	if request.method == "POST":		
+		if 'cart' not in request.session:
+			request.session['cart'] = []
+		id_part = request.POST.get('id_part', None)
+		request.session['cart'].append({'id_part': id_part, 'cant': 0, })
+
+	q = request.GET.get('q', None)
+	page = request.GET.get('page', 1)
+	category = request.GET.get('category', None)
+		
+	parts_list = Part.objects.all()
+		
+	if category != None and category != '0':
+		parts_list = parts_list.filter(category=category)
+	else:
+		category = '0'
+
+	if q != None and q != '':
+		parts_list = parts_list.filter(description__contains=q.strip())
+	else:
+		q = ''
+
+	paginator = Paginator(parts_list, 1)
+	parts = paginator.page(page)
+		
+	return render(request, 'parts.html', {'parts' : parts, 'category': category, 'q':q})	
 
 
 @login_required()
