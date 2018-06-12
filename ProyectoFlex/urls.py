00591e84c9f5c390d6f415c82952430a079909b7
Luis Iturrios <iturrios3063@gmail.com>
2018-06-12 15:45:18 -0700

    index redirect to toolcrib

--- ProyectoFlex/urls.py
+++ ProyectoFlex/urls.py
@@ -17,11 +17,13 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 
 from toolcrib import urls
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/toolcrib/'), name='index'),# Index Redirect
     url(r'^admin/', admin.site.urls),
     url(r'^toolcrib/', include(urls, namespace='toolcrib'))
 ]
